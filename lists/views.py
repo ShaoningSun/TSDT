c0f10879db33b845e436072d7dd3079aa5782ae2
shaoningsun <shaoningsun@qq.com>
2024-04-25 15:59:06 +0800

    message 16: form URL of new list by POST

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-new-page/')
-
     return render(request, 'home.html')
 
 # Create your views here.
@@ -16,3 +12,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-new-page/')
