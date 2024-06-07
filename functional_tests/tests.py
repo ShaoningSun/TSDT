ec97fd177b11e90bcdc7847ea7d32f07707ec8a1
shaoningsun <shaoningsun@qq.com>
2024-06-08 00:06:58 +0800

    message 20: testing fot layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,6 +16,25 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_layout_and_styling(self):
+        # 张三访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 他看到输入框完美的居中显示
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
+        # 他新建了一个清单，看到输入框完美的居中显示
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
